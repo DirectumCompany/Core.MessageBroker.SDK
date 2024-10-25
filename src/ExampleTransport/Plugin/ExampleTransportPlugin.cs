d223dc8b6c578bf03b7f0a8f7ede9dcbbfffd673
Морозов Андрей (Morozov_AV) <Morozov_AV@directum.ru>
2024-10-25 17:12:35 +0400

    docs: расширил документацию ExampleTransport
    - раскрыл все <inheritdoc/>
    - раскрыл поля базового класса
    - постарался описать особенности и что для чего нужно
    
    ref: #48859, #50248

--- src/ExampleTransport/Plugin/ExampleTransportPlugin.cs
+++ src/ExampleTransport/Plugin/ExampleTransportPlugin.cs
@@ -9,25 +9,60 @@ namespace ExampleTransport.Plugin
   /// <summary>
   /// Транспортный плагин.
   /// </summary>
+  /// <remarks>
+  /// Точка входа для работы с плагином.
+  /// </remarks>
   [Plugin(PluginType = typeof(IMessageTransportPlugin))]
   public class ExampleTransportPlugin : IMessageTransportPlugin
   {
     /// <summary>
     /// Сервис транспортного посредника.
     /// </summary>
+    /// <remarks>
+    /// Атрибут <see cref=" PluginServiceAttribute"/> говорит о том,
+    /// что нужно получить эту зависимость из загрузчика <see cref="ExampleTransportPluginBootstrapper"/>.
+    /// </remarks>
     [PluginService(ProvidedBy = ProvidedBy.Plugin, ServiceType = typeof(IMessageTransportProxy))]
     private readonly IMessageTransportProxy _messageTransportProxy;
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Передаёт сообщение.
+    /// </summary>
+    /// <param name="message">Передаваемое сообщение.</param>
     public async Task TransmitAsync(Message message) => await _messageTransportProxy.TransmitAsync(message);
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Возвращает количество сообщений в секунду, которые позволяет отправлять транспорт.
+    /// </summary>
+    /// <returns>
+    /// Количество сообщений в секунду, которые позволяет отправлять транспорт.
+    /// </returns>
+    /// <remarks>
+    /// Значение <c>0</c> означает "без ограничений".
+    /// </remarks>
     public int GetMessagesPerSecond() => _messageTransportProxy.GetMessagesPerSecond();
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Возвращает количество попыток отправить сообщение.
+    /// </summary>
+    /// <returns>
+    /// Количество попыток отправить сообщение.
+    /// </returns>
+    /// <remarks>
+    /// Значение <c>null</c> означает "без ограничений"
+    /// </remarks>
     public int? GetTrySendCount() => _messageTransportProxy.GetTrySendCount();
 
-    /// <inheritdoc/>
+    /// <summary>
+    /// Выполняет проверку здоровья.
+    /// </summary>
+    /// <returns>
+    /// Результат проверки здоровья.
+    /// </returns>
+    /// <remarks>
+    /// Метод вызывается в общей проверки готовности хоста (/ready) и позволяет быстро определить
+    /// все ли в порядке с транспортным посредником.
+    /// </remarks>
     public async Task<HealthCheckResult> HealthCheck() => await _messageTransportProxy.HealthCheck();
   }
 }
